999c98da06621eb64da444be4df8d7268af06315
语非虫 <7802180+qixianshengcc@user.noreply.gitee.com>
2020-08-25 22:20:58 +0800

    22.20

---
+++ src/test/java/com/qbh/app/Demo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qbh.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
